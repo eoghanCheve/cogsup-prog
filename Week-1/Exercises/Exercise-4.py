b233e42827e8e6f5df6e7ff16d092476c2b03074
Eoghan Cheve <41449253+eoghanCheve@users.noreply.github.com>
2025-09-17 17:43:02 +0200

    Update Exercise-4.py

--- Week-1/Exercises/Exercise-4.py
+++ Week-1/Exercises/Exercise-4.py
@@ -18,7 +18,10 @@ dct = {'a': 3, 'b': 7, 'c': -2, 'd': 10, 'e': 5}
 
 print("Exercise 4.1")
 
-pass
+s = 0
+for a in dct:
+    s += dct[a]
+print(s)
 
 print("---")
 
@@ -32,7 +35,16 @@ Print the key that has the largest value in dct.
 
 print("Exercise 4.2")
 
-pass
+s = 0
+flag = True
+for a in dct:
+    if flag :
+      s = a
+      flag = False
+    if dct[a] > dct[s]:
+       s = a
+print(s)
+
 
 print("---")
 
@@ -46,7 +58,10 @@ Create a new dictionary with the squares of all the values in dct.
 
 print("Exercise 4.3")
 
-pass
+s = {}
+for a in dct:
+    s[a] = dct[a]**2
+print(s)
 
 print("---")
 
@@ -60,7 +75,9 @@ Print only the keys in dct whose values are even numbers.
 
 print("Exercise 4.4")
 
-pass
+for a in dct:
+    if dct[a] % 2 == 0 :
+        print(a)
 
 print("---")
 
@@ -74,7 +91,10 @@ Create a new dictionary that swaps the keys and values in dct.
 
 print("Exercise 4.5")
 
-pass
+s = {}
+for a in dct:
+    s[dct[a]] = a
+print(s)
 
 print("---")
 
@@ -91,7 +111,13 @@ s = 'ccctcctttttcc'
 
 print("Exercise 4.6")
 
-pass
+dic = {}
+for a in s:
+    if a in dic :
+        dic[a] += 1
+    else :
+        dic[a] = 0
+print(dic)
 
 print("---")
 
@@ -110,7 +136,10 @@ responses = 'jjjpjjpppppjj'
 
 print("Exercise 4.7")
 
-pass
+rep = ""
+for a in responses:
+    rep += responses_mapping[a]
+print(rep)
 
 print("---")
 
@@ -125,7 +154,12 @@ Merge the following two dictionaries into one:
 
 print("Exercise 4.8")
 
-pass
+dic0 = {'a': 1, 'b': 2}
+dic1 = {'c': 3, 'd': 4}
+
+for a in dic1 :
+    dic0[a] = dic1[a]
+print(dic0)
 
 print("---")
 
@@ -140,7 +174,9 @@ create a new one whose keys are sorted alphabetically.
 
 print("Exercise 4.9")
 
-pass
+dic = {'zebra': 10, 'dolphin': 25, 'alligator': 3, 'monkey': 5, 'pig': 9}
+dic1 = {k: dic[k] for k in sorted(dic)}
+print(dic1)
 
 print("---")
 
@@ -155,6 +191,16 @@ create a new one whose values appear in increasing order.
 
 print("Exercise 4.10")
 
-pass
+dic = {'zebra': 10, 'dolphin': 25, 'alligator': 3, 'monkey': 5, 'pig': 9}
+
+def swap(dct):
+    s = {}
+    for a in dct:
+        s[dct[a]] = a
+    return s
+
+dic1 = swap({k: swap(dic)[k] for k in sorted(swap(dic))})
+
+print(dic1)
 
 print("---")
