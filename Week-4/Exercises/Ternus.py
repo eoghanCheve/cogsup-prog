e658de19a51c8318237535c5d02395da5f213129
Eoghan Cheve <41449253+eoghanCheve@users.noreply.github.com>
2025-10-01 16:34:48 +0200

    half of week 4 ex 4

---
+++ Week-4/Exercises/Ternus.py
@@ -0,0 +1,50 @@
+from expyriment import design, control, stimuli
+import random
+
+def load(stims):
+    for stim in stims: 
+        stim.preload()
+def timed_draw(elements):
+    n = len(elements)
+    if n == 1:
+        elements[0].present(clear=True, update=True)
+    else:
+        elements[0].present(clear=True, update=False)
+        for i in range(1,n-1):
+            elements[i].present(clear=False, update=False)
+        elements[-1].present(clear=False, update=True)
+    # return the time it took to draw
+
+def present_for(elements, t=1000):
+    n = len(elements)
+    t0 = exp.clock.time
+    if n == 1:
+        elements[0].present(clear=True, update=True)
+    else:
+        elements[0].present(clear=True, update=False)
+        for i in range(1,n-1):
+            elements[i].present(clear=False, update=False)
+        elements[-1].present(clear=False, update=True)
+    t1 = exp.clock.time - t0
+    exp.clock.wait(t - t1)
+
+def ternus(ISI = 0, color = False, radius = 50):
+    circles = [stimuli.Circle(colour="blue", position=((i*radius*2.5)-3.75
+                                                      *radius,0), radius=radius) for i in range(4)]
+    load(circles)
+    for i in range(10):
+        present_for(circles[0:3], 200)
+        exp.clock.wait(ISI)
+        present_for(circles[1:4], 200)
+
+""" Test functions """
+exp = design.Experiment()
+
+control.set_develop_mode()
+control.initialize(exp)
+
+ternus(0, False)
+ternus(50, False)
+
+
+control.end()
